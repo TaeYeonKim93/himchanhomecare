81ea69c708dacb9cf667afbb473514eb792fd9bd
TaeyeonKim93 <taeyeon.dev@gmail.com>
2024-12-08 14:26:00 +0900

    config 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import fs from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  server: {
+    https: {
+      key: fs.readFileSync('/etc/letsencrypt/live/himchan.hopto.org/privkey.pem'),
+      cert: fs.readFileSync('/etc/letsencrypt/live/himchan.hopto.org/fullchain.pem'),
+    },
+    host: true,
+    port: 443
   },
 });
