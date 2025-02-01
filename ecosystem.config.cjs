f7f5ffe5692be7ad13ad2ab8117068fb882b1309
TaeYeonKim93 <taeyeonkim93@gmail.com>
2025-02-01 15:17:47 +0900

    Resolve package.json merge conflicts

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -11,7 +11,6 @@ module.exports = {
     exec_interpreter: 'none',
     exec_mode: 'fork',
     exec_user: 'root',
-    
     max_memory_restart: '1G',
     // Git과 빌드 자동화 설정
     watch: ["src"],  // 특정 디렉토리만 감시
@@ -22,8 +21,8 @@ module.exports = {
     },
     autorestart: true,
     exp_backoff_restart_delay: 100,
-    // 파일 변경 감지시 실행할 스크립트
-    watch_exec: "npm install && npm run build && git pull",
+    // 파일 변경 감지시 실행할 스크립트 (순서 변경)
+    watch_exec: "git pull && npm install && npm run build",
     // 오류 발생시 재시작 관련 설정
     max_restarts: 10,
     restart_delay: 3000
