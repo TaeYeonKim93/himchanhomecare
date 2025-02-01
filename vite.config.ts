6f8e06c0c45127a518b5a85ffc89a7e3d8253e3b
김태연 <sjdksi125@gmail.com>
2025-02-01 15:08:31 +0900

    google search console

--- vite.config.ts
+++ vite.config.ts
@@ -40,6 +40,7 @@ export default defineConfig(({ mode }) => {
       outDir: 'dist',
       assetsDir: 'assets',
       sourcemap: env.VITE_ENABLE_SOURCEMAP === 'true'
-    }
+    },
+    base: '/',
   };
 });
